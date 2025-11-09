18b00c5e1c142f70cfee9ee17264f8cc232c6184
Zhidkov-Aleksandr <dotenvargen@gmail.com>
2025-11-09 17:56:28 +0300

    new commit

--- src/main/java/com/example/ORMProject/model/Course.java
+++ src/main/java/com/example/ORMProject/model/Course.java
@@ -29,6 +29,14 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Course {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
